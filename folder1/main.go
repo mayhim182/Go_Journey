d6665155425c0e317b50c85dc371d358064a8e57
mayhim182 <mayankatnitrkl@gmail.com>
2023-11-13 17:30:07 +0530

    Go Slices and Arrays

--- folder1/main.go
+++ folder1/main.go
@@ -52,6 +52,7 @@ func constant() {
 	fmt.Println(math.Sin(n))
 
 	forLoopTut()
+	arrayPrac()
 
 }
 
@@ -78,4 +79,26 @@ func forLoopTut() {
 		}
 		fmt.Println(n)
 	}
+}
+
+// Go arrays
+
+func arrayPrac() {
+	var a [5]int
+	fmt.Println("Emp: ", a)
+	a[4] = 100
+	fmt.Println("Set: ", a)
+	fmt.Println("Get: ", a[4])
+	fmt.Println("len: ", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl: ", b)
+
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2d: ", twoD)
 }
